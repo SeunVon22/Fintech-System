79ce3bc50e45c2d9e507d6f003e2a090e669054a
isaiahvaughan9@gmail.com <isaiahvaughan9@gmail.com>
2026-05-20 11:31:07 +0100

    fix

--- payment-service-complete/payment-service/terraform/main.tf
+++ payment-service-complete/payment-service/terraform/main.tf
@@ -148,8 +148,8 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   # ── RBAC ─────────────────────────────────────────────────────────────────
   azure_active_directory_role_based_access_control {
-    managed            = true
     azure_rbac_enabled = true
+    tenant_id          = data.azurerm_client_config.current.tenant_id
   }
 
   # ── Auto-upgrade ──────────────────────────────────────────────────────────
